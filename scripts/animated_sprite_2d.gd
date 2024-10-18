7201d142fbb9e45f8d9cfd60f793ac05c013eb24
AndreasHujber <S1710454010@students.fh-hagenberg.at>
2024-10-18 20:39:24 +0200

    add cabines and map stuff

---
+++ scripts/animated_sprite_2d.gd
@@ -0,0 +1,11 @@
+extends AnimatedSprite2D
+class_name clothing_rack
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var rng = RandomNumberGenerator.new()
+	frame = rng.randi_range(0,7)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
