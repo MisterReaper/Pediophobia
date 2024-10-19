e41c138f2e556ddb5bde150369d2c12d49a92b61
MisterReaper <88578229+MisterReaper@users.noreply.github.com>
2024-10-19 13:47:35 +0200

    Added mannequin + moved ui assets around

---
+++ scripts/mobs/mannequin.gd
@@ -0,0 +1,76 @@
+extends CharacterBody2D
+
+
+signal kills
+var target
+
+@onready var states: StateChart = $States
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audioPlayer = $AudioStreamPlayer2D
+const SPEED = 40.0
+const directions = ["down", "left", "right", "up"]
+var direction = directions[0]
+var debugnode
+
+func _ready():
+	add_to_group("enemy")
+	if OS.is_debug_build():
+		debugnode = $debugInfoTemplate
+		debugnode.visible = true
+		self.remove_child(debugnode)
+		self.get_parent().call_deferred("add_child", debugnode)
+
+func _physics_process(delta):
+	if target:
+		var target_pos = target.global_position
+		velocity = global_position.direction_to(target_pos) * SPEED
+		if velocity.y:
+			direction = "down" if velocity.y > 0.0 else "up"
+		else:
+			direction = "left" if velocity.x < 0.0 else "right"
+		states.send_event("run")
+		playAnimation("run_" + direction)
+		move_and_slide()
+		if randi_range(0,3000) == 3000:
+			#audioPlayer.stream = load("res://assets/sounds/chaserGhost_idle.mp3")
+			#audioPlayer.play()
+			pass
+		if debugnode:
+			debugnode.position = target_pos
+	else:
+		states.send_event("idle")
+	
+func playAnimation(anim):
+	if sprite.animation != anim:
+		var frame = sprite.frame
+		sprite.play(anim)
+		sprite.frame = frame
+
+func resetAnimation():
+	sprite.frame = 0
+
+func _on_idle_state_entered():
+	resetAnimation()
+	playAnimation("idle_" + direction)
+
+
+func _on_aggro_range_area_entered(body):
+	
+	if body.name == "InteractionBox":
+		target = body
+		audioPlayer.stream = load("res://assets/sounds/chaserGhost_take_chase.mp3")
+		audioPlayer.play()
+		print_debug("Target spotted")
+
+
+func _on_aggro_range_area_exited(body):
+	
+	if body.name == "InteractionBox":
+		target = null
+		print_debug("Target lost")
+
+
+func _on_hit_box_area_entered(body):
+	print_debug("Body entered: "+ body.name)
+	if body.name == "WholeBodyBox":
+		kills.emit("chaserGhost")
