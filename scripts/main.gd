e4546d18534b474563a35f57268c9a13b3c6eb15
MisterReaper <88578229+MisterReaper@users.noreply.github.com>
2024-10-19 21:47:27 +0200

    removed light-darkness setup on main start

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,8 +4,8 @@ extends Node2D
 @onready var player = $MainCharacter
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	darkness.energy = 12
-	player.flashlight.energy = 1
+	#darkness.energy = 12
+	#player.flashlight.energy = 1
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
