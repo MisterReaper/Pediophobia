4fb5ae711443aed7d00d7bcdea5fac3e0c823e09
MisterReaper <88578229+MisterReaper@users.noreply.github.com>
2024-10-19 14:39:19 +0200

    added mannequinn to scene currently can only detect players to thhe south

--- scripts/mobs/mannequin.gd
+++ scripts/mobs/mannequin.gd
@@ -14,7 +14,7 @@ var debugnode
 
 func _ready():
 	add_to_group("enemy")
-	if OS.is_debug_build():
+	if false && OS.is_debug_build():
 		debugnode = $debugInfoTemplate
 		debugnode.visible = true
 		self.remove_child(debugnode)
@@ -38,7 +38,8 @@ func _physics_process(delta):
 		if debugnode:
 			debugnode.position = target_pos
 	else:
-		states.send_event("idle")
+		#states.send_event("idle")
+		pass
 	
 func playAnimation(anim):
 	if sprite.animation != anim:
@@ -53,24 +54,23 @@ func _on_idle_state_entered():
 	resetAnimation()
 	playAnimation("idle_" + direction)
 
-
-func _on_aggro_range_area_entered(body):
-	
-	if body.name == "InteractionBox":
-		target = body
-		audioPlayer.stream = load("res://assets/sounds/chaserGhost_take_chase.mp3")
-		audioPlayer.play()
-		print_debug("Target spotted")
-
-
-func _on_aggro_range_area_exited(body):
-	
-	if body.name == "InteractionBox":
-		target = null
-		print_debug("Target lost")
-
-
 func _on_hit_box_area_entered(body):
 	print_debug("Body entered: "+ body.name)
 	if body.name == "WholeBodyBox":
 		kills.emit("chaserGhost")
+
+
+func _on_detection_area_area_entered(area: Area2D) -> void:
+	print_debug("Body entered: "+ area.name)
+	if area.name == "InteractBox":
+		target = area
+		#audioPlayer.stream = load("res://assets/sounds/chaserGhost_take_chase.mp3")
+		#audioPlayer.play()
+		print_debug("Target spotted")
+
+
+func _on_detection_area_area_exited(area: Area2D) -> void:
+	print_debug(area.name)
+	if area.name == "InteractBox":
+		target = null
+		print_debug("Target lost")
