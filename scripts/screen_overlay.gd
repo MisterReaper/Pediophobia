6ad2de8e1a965fe2d80f99e05b0202d76da1425a
MisterReaper <88578229+MisterReaper@users.noreply.github.com>
2024-10-20 13:24:09 +0200

    Added Key object and changed layout for camera overlay

--- scripts/screen_overlay.gd
+++ scripts/screen_overlay.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+@onready var objectiveText = $HBoxContainer/ObjectiveText
+@onready var container = $HBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +11,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func changeObjective(text):
+	objectiveText.text = text
+
+func addToContainer(object):
+	container.add_child(object)
+
+func removeFromContainer(object):
+	container.remove_child(object)
