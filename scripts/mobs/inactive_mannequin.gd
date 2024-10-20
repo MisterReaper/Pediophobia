0388cfbd7bd617beca7eea586ff68330ee810afe
MisterReaper <88578229+MisterReaper@users.noreply.github.com>
2024-10-20 18:25:45 +0200

    Inactive_mannequin proper setup for random versions

--- scripts/mobs/inactive_mannequin.gd
+++ scripts/mobs/inactive_mannequin.gd
@@ -1,13 +1,12 @@
-extends CharacterBody2D
+extends AnimatedSprite2D
 
 
 const SPEED = 0.0
 const JUMP_VELOCITY = -400.0
-@onready var Sprite = $Sprite
 
 func _ready() -> void:
 	var rng = RandomNumberGenerator.new()
-	Sprite.frame = rng.randi_range(0,4)
+	frame = rng.randi_range(0,4)
 	
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
