cfc83882289d4a258653e1b2f19b1a46363761c3
MisterReaper <88578229+MisterReaper@users.noreply.github.com>
2024-10-21 20:56:19 +0200

    Removed credits link from title, added title_screen button to credits, added empty texture panel to credits

--- scenes/credits.gd
+++ scenes/credits.gd
@@ -1,5 +1,5 @@
 extends Control
-var simultaneous_scene = preload("res://scenes/main.tscn").instantiate()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,8 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
+func _on_back_to_title_pressed() -> void:
+	get_tree().change_scene_to_file("res://system/title_screen.tscn")
