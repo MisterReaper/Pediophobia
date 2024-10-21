238e5c24775b6da8b608fc618ba4db6d139de96c
AndreasHujber <S1710454010@students.fh-hagenberg.at>
2024-10-21 20:56:15 +0200

    sounds sounds sounds

--- scripts/tonys_bag.gd
+++ scripts/tonys_bag.gd
@@ -1,6 +1,6 @@
 extends Sprite2D
 signal bagIsPickedUp
-
+@onready var audioPlayer = $AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,6 +11,16 @@ func _process(delta: float) -> void:
 	pass
 
 func interact():
-	queue_free() # function gets still completed don't worry ;D
-	emit_signal("bagIsPickedUp")
+	if not audioPlayer.playing:
+		var audiostream: AudioStream = preload("res://assets/sounds/bag-grab.mp3")
+		audioPlayer.set_stream(audiostream)
+		audioPlayer.play()
+		emit_signal("bagIsPickedUp")
+	
+	
 	return ["Fucking lights went out."]	
+
+
+func _on_audio_stream_player_finished() -> void:
+	queue_free() # function gets still completed don't worry ;D
+	pass # Replace with function body.
