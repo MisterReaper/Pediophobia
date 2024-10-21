e5a8f5d8e8d21ab6c659e45bd418729299f3d93e
AndreasHujber <S1710454010@students.fh-hagenberg.at>
2024-10-21 20:03:59 +0200

    fix key sound

--- scripts/front_key.gd
+++ scripts/front_key.gd
@@ -12,9 +12,15 @@ func _process(delta: float) -> void:
 	pass
 	
 func interact():
-	queue_free() # function gets still completed don't worry ;D
-	var audiostream: AudioStream = preload("res://assets/sounds/key-grab.mp3")
-	audioPlayer.set_stream(audiostream)
-	audioPlayer.play()
+	if not audioPlayer.playing:
+		var audiostream: AudioStream = preload("res://assets/sounds/key-grab.mp3")
+		audioPlayer.set_stream(audiostream)
+		audioPlayer.play()
 	return ["Must be the front door key."]
+
 	
+
+
+func _on_audio_stream_player_finished() -> void:
+	queue_free() # function gets still completed don't worry ;D
+	pass # Replace with function body.
