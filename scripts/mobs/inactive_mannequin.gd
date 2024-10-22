d76b273c2eff0db333f4ceb15844c63acdcdf481
MisterReaper <88578229+MisterReaper@users.noreply.github.com>
2024-10-22 20:26:54 +0200

    inactive_mannequin added to kill list

--- scripts/mobs/inactive_mannequin.gd
+++ scripts/mobs/inactive_mannequin.gd
@@ -7,6 +7,10 @@ const JUMP_VELOCITY = -400.0
 func _ready() -> void:
 	var rng = RandomNumberGenerator.new()
 	frame = rng.randi_range(0,4)
+	add_to_group("inactive_mannequin")
 	
 func _process(delta: float) -> void:
 	pass
+
+func removeSelf():
+	queue_free()
